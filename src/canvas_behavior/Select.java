ac1e739a4baec0dc99ecb4df54f57456765723a9
Xyphuz <hansen0715.2@gmail.com>
2021-04-06 11:56:20 +0800

    wip: Select area.

--- src/canvas_behavior/Select.java
+++ src/canvas_behavior/Select.java
@@ -1,11 +1,18 @@
 package canvas_behavior;
 
+import java.awt.Color;
+import java.util.Random;
+
+import javax.swing.JLabel;
+
 import components.UMLObjects.BaseUMLObject;
 import widgets.Canvas;
 
 public class Select implements ICanvasBehavior {
     protected int originalX;
     protected int originalY;
+
+    protected JLabel selectedArea;
 
     @Override
     public void onPressed(int mousePosX, int mousePosY) {
@@ -20,7 +27,27 @@ public class Select implements ICanvasBehavior {
             Canvas.clearSelections();
             Canvas.addSelection(within);
         } else {
+            this.originalX = mousePosX;
+            this.originalY = mousePosY;
+
             Canvas.clearSelections();
+
+            Random random = new Random();
+
+            float r = random.nextFloat();
+            float g = random.nextFloat();
+            float b = random.nextFloat();
+
+            Color randomColor = new Color(r, g, b);
+            randomColor.brighter();
+
+            this.selectedArea = new JLabel();
+            this.selectedArea.setOpaque(true);
+            this.selectedArea.setBackground(randomColor);
+            this.selectedArea.setBounds(this.originalX, this.originalY, 0, 0);
+
+            Canvas.getInstance().addUMLObject(selectedArea, -1);
+            Canvas.getInstance().repaint();
         }
     }
 
@@ -32,11 +59,18 @@ public class Select implements ICanvasBehavior {
         if (Canvas.getSelections().size() == 1) {
             Canvas.getSelections().iterator().next().setLocation(mousePosX - originalX, mousePosY - originalY);
             Canvas.getInstance().repaint();
+        } else {
+            this.selectedArea.setBounds(Math.min(this.originalX, mousePosX), Math.min(this.originalY, mousePosY), Math.abs(this.originalX - mousePosX), Math.abs(this.originalY - mousePosY));
+
+            Canvas.getInstance().repaint();
         }
     }
 
     @Override
     public void onReleased(int mousePosX, int mousePosY) {
         System.out.println("SelectMode onReleased");
+
+        Canvas.getInstance().remove(this.selectedArea);
+        Canvas.getInstance().repaint();
     }
 }
